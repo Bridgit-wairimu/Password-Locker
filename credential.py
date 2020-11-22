0dcc3f23d51f336b4ec6946cd279e343425e7eb6
Bridgit-wairimu <kirikabetty@gmail.com>
2020-11-22 13:48:21 +0300

    added some new tests to check whether the app is working

--- credential.py
+++ credential.py
@@ -2,6 +2,8 @@ class Credential:
     """
     a class that generates new instances of credentials
     """
+
+    credential_list= []
     
     def __init__(self,Gmail,username,password):
         """
@@ -11,5 +13,19 @@ class Credential:
         self.username = username
         self.password = password
 
+
+    def save_credential(self):
+        """
+        saves credentail objects into credential list
+        """
+        Credential.credential_list.append(self)
+
         
-        
+    @classmethod
+    def find_credential_by_username(cls,username):
+        """
+        this method takes in username and returns a username that matches the username
+        """
+        for credential in cls.credential_list:
+            if credential.username == username:
+                return credential
--- credential_test.py
+++ credential_test.py
@@ -4,7 +4,7 @@ from credential import Credential
 class TestCredential(unittest.TestCase):
 
     def setUp(self):
-        self.new_credential = credential("kirikabridgit@gmail.com","br254","nims67&")
+        self.new_credential = Credential("kirikabridgit@gmail.com","br254","nims67&")
         
     def test_unit(self):
         """
@@ -13,6 +13,49 @@ class TestCredential(unittest.TestCase):
         self.assertEqual(self.new_credential.Gmail,"kirikabridgit@gmail.com")
         self.assertEqual(self.new_credential.username,"br254")
         self.assertEqual(self.new_credential.password,"nims67&")
+
+
+    def test_save_credential(self):
+        """
+        this method tests if the user object is saved in the credential list
+        """
+        self.new_credential.save_credential() # saves the new list
+        self.assertEqual(len(Credential.credential_list),1)
+
+    def test_save_multiple_credential(self):
+        """
+        checks if we can save multiple credentials
+        """
+
+        self.new_credential.save_credential()
+        second_credential = Credential("Instagram","nimoh916","nims65$*")
+        second_credential.save_credential()
+        self.assertEqual(len(Credential.credential_list),2)
+
+
+    def tearDown(second_credential):
+        """
+        tearDown is a method that cleans up after each teat runs.
+        """
+        Credential.credential_list = []
+
+    def test_find_credential_by_username(self):
+        """
+        method checks if we can find a credential by username
+        """
+
+        second_credential = Credential("Instagram","nimoh916","nims65$*")
+        second_credential.save_credential()
+
+        found_credential = Credential.find_credential_by_username("nimoh916")
+        self.assertEqual(found_credential.username,second_credential.username)
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
