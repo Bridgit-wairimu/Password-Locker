d9041f1d3aa7a8a823873bd5d6f7a3cd2025f280
Bridgit-wairimu <kirikabetty@gmail.com>
2020-11-22 16:22:56 +0300

    deleted the credential class and modified the password class

--- password_test.py
+++ password_test.py
@@ -1,5 +1,6 @@
 import unittest
 from password import User
+from password import Credential
 
 class TestPassword(unittest.TestCase):
 
@@ -59,7 +60,7 @@ class TestPassword(unittest.TestCase):
             second_user.save_user()
 
             self.new_user.delete_user()
-            self.assertEqual(len(User.user_list),2)
+            self.assertEqual(len(User.user_list),1)
 
     def test_user_exists(self):
             """
@@ -78,6 +79,89 @@ class TestPassword(unittest.TestCase):
             this method returns a list of all users saved
             """
             self.assertEqual(User.display_user(),User.user_list)
+
+
+class TestCredential(unittest.TestCase):
+
+    def setUp(self):
+        self.new_credential = Credential("kirikabridgit@gmail.com","br254","nims67&")
+        
+    def test_unit(self):
+        """
+        this method tests if the credential is saved into the credentiallist
+        """
+        self.assertEqual(self.new_credential.Gmail,"kirikabridgit@gmail.com")
+        self.assertEqual(self.new_credential.username,"br254")
+        self.assertEqual(self.new_credential.password,"nims67&")
+
+
+    def test_save_credential(self):
+        """
+        this method tests if the user object is saved in the credential list
+        """
+        self.new_credential.save_credential() # saves the new list
+        self.assertEqual(len(Credential.credential_list),1)
+
+    def test_save_multiple_credential(self):
+        """
+        checks if we can save multiple credentials
+        """
+
+        self.new_credential.save_credential()
+        second_credential = Credential("Instagram","nimoh916","nims65$*")
+        second_credential.save_credential()
+        self.assertEqual(len(Credential.credential_list),2)
+
+
+    def tearDown(second_credential):
+        """
+        tearDown is a method that cleans up after each teat runs.
+        """
+        Credential.credential_list = []
+
+    def test_find_credential_by_username(self):
+        """
+        method checks if we can find a credential by username
+        """
+        self.new_credential.save_credential()
+        second_credential = Credential("Instagram","nimoh916","nims65$*")
+        second_credential.save_credential()
+
+        found_credential = Credential.find_credential_by_username("nimoh916")
+        self.assertEqual(found_credential.username,second_credential.username)
+
+
+    def test_delete_credential(self):
+        """
+        this method tests whether we can remove a credential from our credentail list.
+        """
+        self.new_credential.save_credential()
+        second_credential = Credential("Instagram","nimoh916","nims65$*")
+        second_credential.save_credential()
+
+        self.new_credential.delete_credential()
+        self.assertEqual(len(Credential.credential_list),1)
+
+
+    def test_credential_exists(self):
+        """
+        tests to check if we can return a boolean if we cannot find the credential
+        """
+        self.new_credential.save_credential()
+        second_credential = Credential("Instagram","nimoh916","nims65$*")
+        second_credential.save_credential()
+
+        credential_exists = Credential.credential_exists("nimoh916")
+        self.assertTrue(credential_exists)
+
+
+    def test_display_all_users(self):
+        """
+        this method returns a list of all credentials saved
+        """
+        self.assertEqual(Credential.display_credential(),Credential.credential_list)
+
+       
 
     if __name__ == '__main__':
         unittest.main()
--- run.py
+++ run.py
@@ -1,1 +1,42 @@
 #!/usr/bin/env python
+from password import User
+from credential import Credential
+
+def create_new_user(fname,lname,password):
+    """
+    function to create a new user
+    """
+    new_user = User(fname,lname,password)
+    return new_user
+
+def save_users(user):
+    """
+    function to save user
+    """
+    user.save_user()
+
+def del_user(user):
+    """
+    function to delete a user
+    """
+    user.delete_user()
+
+def find_user(password):
+    """
+    function that finds a user by password and returns the user
+    """
+    return User.find_user_by_password(password)
+
+def check_existing_user(password):
+    """
+    function that check if a user exists with that a password return a boolean
+    """
+    return User.user_exist(password)
+
+    if __name__ == '__main__':
+        password()           
+            
+
+
+
+
